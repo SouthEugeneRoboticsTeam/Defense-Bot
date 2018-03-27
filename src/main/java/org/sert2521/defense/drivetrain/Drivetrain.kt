38e3ca88a6a519fe9723c9a40c676aff7b32375f
Aidan Heller <asfheller@gmail.com>
2018-03-27 16:25:15 -0700

    Update libs and drivetrain to match

--- src/main/java/org/sert2521/defense/drivetrain/Drivetrain.kt
+++ src/main/java/org/sert2521/defense/drivetrain/Drivetrain.kt
@@ -1,5 +1,6 @@
 package org.sert2521.defense.drivetrain
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
 import edu.wpi.first.wpilibj.drive.DifferentialDrive
 import org.sert2521.defense.util.LEFT_FRONT_MOTOR
 import org.sert2521.defense.util.LEFT_REAR_MOTOR
@@ -7,30 +8,24 @@ import org.sert2521.defense.util.RIGHT_FRONT_MOTOR
 import org.sert2521.defense.util.RIGHT_REAR_MOTOR
 import org.sert2521.defense.util.leftJoystick
 import org.sertain.command.Subsystem
-import org.sertain.hardware.Talon
-import org.sertain.hardware.autoBreak
-import org.sertain.hardware.inverted
-import org.sertain.hardware.plus
-import org.sertain.hardware.resetEncoder
+import org.sertain.hardware.*
 
 object Drivetrain : Subsystem() {
-    private val frontLeft = Talon(LEFT_FRONT_MOTOR) + Talon(LEFT_REAR_MOTOR)
-    private val frontRight = Talon(RIGHT_FRONT_MOTOR) + Talon(RIGHT_REAR_MOTOR)
+    private val leftDrive = Talon(LEFT_FRONT_MOTOR).autoBreak() + Talon(LEFT_REAR_MOTOR).autoBreak()
+    private val rightDrive = Talon(RIGHT_FRONT_MOTOR).autoBreak() + Talon(RIGHT_REAR_MOTOR).autoBreak()
 
-    private val drive = DifferentialDrive(frontLeft, frontRight)
+    private val drive = DifferentialDrive(leftDrive, rightDrive)
 
     override val defaultCommand = ArcadeDrive()
 
-    init {
-        frontLeft.autoBreak()
-        frontRight.autoBreak()
+    override fun onCreate() {
+        leftDrive.setSelectedSensor(FeedbackDevice.QuadEncoder)
+        rightDrive.setSelectedSensor(FeedbackDevice.QuadEncoder)
     }
 
     override fun onStart() {
-        stop()
-
-        frontLeft.resetEncoder()
-        frontRight.resetEncoder()
+        leftDrive.setEncoderPosition(0)
+        rightDrive.setEncoderPosition(0)
     }
 
     fun arcade() {
