2878b7cc511ecb6dfb15af77007a34202b9c8009
Aidan Heller <asfheller@gmail.com>
2018-03-27 15:46:17 -0700

    Add talon ids

--- src/main/java/org/sert2521/defense/util/Constants.kt
+++ src/main/java/org/sert2521/defense/util/Constants.kt
@@ -5,7 +5,7 @@ const val LEFT_STICK_PORT = 0
 const val RIGHT_STICK_PORT = 1
 
 // Talon IDs
-const val LEFT_FRONT_MOTOR = -1
-const val LEFT_REAR_MOTOR = -1
-const val RIGHT_FRONT_MOTOR = -1
-const val RIGHT_REAR_MOTOR = -1
+const val LEFT_FRONT_MOTOR = 11
+const val LEFT_REAR_MOTOR = 12
+const val RIGHT_FRONT_MOTOR = 13
+const val RIGHT_REAR_MOTOR = 14
